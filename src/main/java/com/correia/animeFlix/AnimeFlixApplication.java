66547ded0ffd8c755606b37c83067aa9a13c42be
Emanoel <emanoeltga@gmail.com>
2025-09-10 21:07:55 -0400

    first commit

---
+++ src/main/java/com/correia/animeFlix/AnimeFlixApplication.java
@@ -0,0 +1,13 @@
+package com.correia.animeFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/correia/animeFlix/AnimeFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correia.animeFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
